5e31f0c55eaa4029b697aca8aec4740d7a30c297
Henry J. Garcia <hjgarcia1@gmail.com>
2021-01-03 12:55:18 -0500

    contact form fixes

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -7,28 +7,28 @@
                     <form wire:submit.prevent="submit" action="/contact" method="POST" class="w-full">
                         @csrf
 
-                        <div class="py-3">
-                            <input wire:model="name" class="w-full px-5 py-3 border border-gray-400 rounded-lg outline-none focus:shadow-outline" type="text" placeholder="Name" name="name" value="{{ old('name') }}" />
+                        <div class="mb-6">
+                            <input wire:model="name" class="w-full px-5 py-3 border border-gray-400 rounded outline-none focus:shadow-outline" type="text" placeholder="Name" name="name" value="{{ old('name') }}" />
                             @error('name')
-                            <p class="text-red-500 mt-1">{{ $message }}</p>
+                            <p class="text-red-500 mt-2 text-sm">{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div class="pb-3">
-                            <input wire:model="email" class="w-full px-5 py-3 border border-gray-400 rounded-lg outline-none focus:shadow-outline" type="text" placeholder="Email Address" name="email" value="{{ old('email') }}" />
+                        <div class="mb-6">
+                            <input wire:model="email" class="w-full px-5 py-3 border border-gray-400 rounded outline-none focus:shadow-outline" type="text" placeholder="Email Address" name="email" value="{{ old('email') }}" />
                             @error('email')
-                            <p class="text-red-500 mt-1">{{ $message }}</p>
+                            <p class="text-red-500 mt-2 text-sm">{{ $message }}</p>
                             @enderror
                         </div>
 
-                        <div class="py-3">
-                            <textarea wire:model="comment" row="4" class="w-full h-40 px-5 py-3 border border-gray-400 rounded-lg outline-none focus:shadow-outline" name="comment" placeholder="Your message here...">{{ old('comment') }}</textarea>
+                        <div class="mb-6">
+                            <textarea wire:model="comment" row="4" class="w-full h-40 px-5 py-3 border border-gray-400 rounded outline-none focus:shadow-outline" name="comment" placeholder="Your message here...">{{ old('comment') }}</textarea>
                             @error('comment')
-                            <p class="text-red-500 mt-1">{{ $message }}</p>
+                            <p class="text-red-500 mt-2 text-sm">{{ $message }}</p>
                             @enderror
                         </div>
-                        <div class="pt-3">
-                            <button class="flex px-6 py-3 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 hover:text-white focus:outline-none focus:shadow-outline focus:border-indigo-300" type="submit">
+                        <div>
+                            <button class="flex px-6 py-3 text-white bg-indigo-500 rounded hover:bg-indigo-600 hover:text-white focus:outline-none focus:shadow-outline focus:border-indigo-300" type="submit">
                                 <span class="self-center float-left text-base font-medium">Submit</span>
                             </button>
                         </div>
