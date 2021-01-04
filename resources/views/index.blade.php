b5f1b9cda6c9a5ce382f2e93467339cfafd50f0b
Henry J. Garcia <hjgarcia1@gmail.com>
2021-01-04 07:16:24 -0500

    added intro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-6xl">Livewire Examples</h1>
+<h1 class="text-6xl mb-4">Livewire Examples</h1>
+<p class="mb-3">The following is a collection of examples for <a href="https://laravel-livewire.com/" target="_blank" class="text-blue-500 hover:underline">Livewire</a>. I will updating this periodically with new examples.</p>
+<p>The idea is to create as many examples as possible with different examples of components used on a everday basis.</p>
+
+<h2 class="text-4xl mt-4 mb-4">Stack</h3>
+<ul class="list-disc ml-5 mt-3">
+    <li>PHP 8</li>
+    <li>MYSQl 5.7 (at least)</li>
+    <li>Mailhog or Mailtrap (depends on settings)</li>
+    <li>Laravel 8</li>
+    <li>TailwindCSS 2</li>
+    <li>Livewire 2</li>
+</ul>
 @endsection
